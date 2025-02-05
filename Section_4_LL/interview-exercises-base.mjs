e3da2c494c698ed0862b9fa5107d6ffba2769999
Iris <i.casinteva@gmail.com>
2025-02-05 13:40:16 +0100

    Interview exercises set up

---
+++ Section_4_LL/interview-exercises-base.mjs
@@ -0,0 +1,55 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+export class LinkedList {
+  constructor(value) {
+    const newNode = new Node(value);
+    this.head = newNode;
+    this.tail = this.head;
+  }
+
+  printList() {
+    let temp = this.head;
+    while (temp !== null) {
+      console.log(temp.value);
+      temp = temp.next;
+    }
+  }
+
+  getHead() {
+    if (this.head === null) {
+      console.log('Head: null');
+    } else {
+      console.log('Head: ' + this.head.value);
+    }
+  }
+
+  getTail() {
+    if (this.tail === null) {
+      console.log('Tail: null');
+    } else {
+      console.log('Tail: ' + this.tail.value);
+    }
+  }
+
+  makeEmpty() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+  }
+}
